5f7375496b4128934e94a438de4480d24a5f5e97
Lukas-Jdk <jdklukac@gmail.com>
2025-07-02 20:24:39 +0200

    created feua19  forum server with mongoDB

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000
+
+app.use(cors());
+app.use(express.json())
+
+const startServer = async () => {
+try { 
+  await mongoose.connect(process.env.MONGODB_URL);
+  console.log("MongoDB loged in susccesfully");
+
+  app.listen(PORT,() => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+}catch(error) {
+  console.error("Failed to login at MongoDB",error);
+}
+};
+
+startServer();
